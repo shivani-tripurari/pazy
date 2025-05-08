05481414d382a30e37d889091fe3736cb6900741
Shivani <shivanitripurari07@gmail.com>
2025-05-08 12:30:58 +0530

    first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Pazy assignment</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
